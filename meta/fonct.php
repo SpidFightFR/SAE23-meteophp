008be8107ab1d9af0122c1c99888d3f0b68ce679
SpidFightFR <60798703+SpidFightFR@users.noreply.github.com>
2022-05-16 14:26:02 +0200

    Update fonct.php

--- meta/fonct.php
+++ meta/fonct.php
@@ -1,7 +1,6 @@
 <?php
 			function meteo($ville){
 			global $climat;
-			//$ville="montcuq";
 			$meteo = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=' . $ville . ',fr&lang=fr&units=metric&APPID=20732b663eb6d3ee95e868da30aa8b43');
 			$json = json_decode($meteo, true);
 			$climat = $json['weather'][0]['description'];
